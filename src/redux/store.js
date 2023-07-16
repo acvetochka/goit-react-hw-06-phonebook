998cfe384046f2ebb8cf6691ab8d37ccc7cd05e2
Alona Kuznietsova <acvetochka@gmail.com>
2023-07-16 18:31:59 +0200

    clear code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,10 @@
-// import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
-// import { devToolsEnhancer } from '@redux-devtools/extension';
-
-// const rootReducer = (state = initialState, action) => {
-//   return state;
-// };
-
-// const enhancer = devToolsEnhancer();
 
 export const store = configureStore({
   reducer: {
     contacts: contactReducer,
-    filter: filterReducer
+    filter: filterReducer,
   },
 });
